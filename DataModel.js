389805046f2baa0661d26de9fffb8475afa41280
Bharat Kumar <kumarbharat661@gmail.com>
2023-02-02 19:52:02 +0530

    changed array to coorect

--- DataModel.js
+++ DataModel.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-const dataSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  question: { type: String, required: true },
-  response: { type: String, required: true },
-});
+const dataSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    question: { type: String, required: true },
+    response: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("DataModel", dataSchema);
