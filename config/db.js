a220a5a5d70d3ac1653aadfa359e533889cf49da
Bharat Kumar <kumarbharat661@gmail.com>
2023-02-04 21:41:31 +0530

    mongoose strictquery added

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const config = require("./config");
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(
       `mongodb+srv://Bharatkumar15:${config.MONGO.PASS}@fooders-api.evs9e.mongodb.net/AutoCaller-Poc?retryWrites=true&w=majority`,
       {
